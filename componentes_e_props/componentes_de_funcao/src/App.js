95b16bd5881ce5e743d0a4b8a7984059a9c15f13
leticia.santos <leticia.santos@shopee.com>
2023-07-04 23:27:18 -0300

    adding new path

---
+++ componentes_e_props/componentes_de_funcao/src/App.js
@@ -0,0 +1,12 @@
+// import "./styles.css";
+import Welcome from "./components/welcome/Welcome";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Olá Letícia</h1>
+      <h2>Vamos codar?</h2>
+      <Welcome />
+    </div>
+  );
+}
